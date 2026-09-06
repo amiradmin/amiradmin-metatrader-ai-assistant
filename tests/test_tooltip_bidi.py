7d1be388865f250888bcb41004bbddc909162b40
Amir Behvandi <amirbehvandi747@gmail.com>
2026-09-06 09:29:10 +0330

    Test isolated mixed-direction tooltip rendering

--- tests/test_tooltip_bidi.py
+++ tests/test_tooltip_bidi.py
@@ -2,11 +2,17 @@ from meta_trader_ai.control_extensions import inject_forex_factory_control
 from meta_trader_ai.control_page import control_page_html
 
 
-def test_control_tooltips_handle_mixed_rtl_ltr_text() -> None:
+def test_control_tooltips_render_rtl_with_isolated_ltr_runs() -> None:
     html = inject_forex_factory_control(control_page_html())
 
-    assert "unicode-bidi:plaintext" in html
+    assert "mixed-bidi tooltip runtime" in html
+    assert ".tipbox{" in html
+    assert ".tipbox bdi{" in html
     assert "direction:rtl" in html
-    assert "text-align:start" in html
-    assert "overflow-wrap:anywhere" in html
-    assert "width:320px" in html
+    assert "unicode-bidi:isolate" in html
+    assert "text-align:right" in html
+    assert "width:360px" in html
+    assert "content:none!important" in html
+    assert "document.createElement('bdi')" in html
+    assert "isolate.dir='ltr'" in html
+    assert "MutationObserver" in html
