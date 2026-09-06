307bcffdb4a95e8be9f7c966888e2a1a6951a7ae
Amir Behvandi <amirbehvandi747@gmail.com>
2026-09-06 13:03:49 +0330

    refactor(ui): route control rendering through AMIR UX overrides

--- src/meta_trader_ai/control_extensions.py
+++ src/meta_trader_ai/control_extensions.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-from .control_extensions_impl import inject_forex_factory_control
+from .control_ui_overrides import inject_forex_factory_control
 
 __all__ = ["inject_forex_factory_control"]
